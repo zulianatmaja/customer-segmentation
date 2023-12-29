7bc2058870f15ec54ff368e25a435e543474148d
zulianatmaja <zulianatmaja@gmail.com>
2023-12-29 16:42:03 +0700

    First commit

---
+++ mall-customer.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+df = pd.read_csv('Mall_Customers.csv')
+
+df.rename(index=str, columns={
+    'Annual Income (k$)' : 'Income',
+    'Spending Score (1-100)' : 'Score'
+}, inplace=True)
+
+X = df.drop(['CustomerID', 'Gender'], axis=1)
+
+st.header("Isi Dataset")
+st.write(df)
